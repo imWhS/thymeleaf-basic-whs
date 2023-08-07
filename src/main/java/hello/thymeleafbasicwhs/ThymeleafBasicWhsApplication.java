3ea3483efd4c7b42cf5ab4f3b628f959092e9c62
Wonhee Son <dnjsgml1230@gmail.com>
2023-08-07 10:47:49 +0900

    initial commit

---
+++ src/main/java/hello/thymeleafbasicwhs/ThymeleafBasicWhsApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleafbasicwhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBasicWhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBasicWhsApplication.class, args);
+	}
+
+}
